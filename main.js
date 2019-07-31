75c81f6f99587b39de8dbbc3d177984d6e8e0396
ycy <568082704@qq.com>
2019-07-31 10:45:49 +0800

    ajax修改

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 myButton.addEventListener('click',function() {
   let request = new XMLHttpRequest
-  request.open('GET','http://jack.com:8002/xxx')
-  request.send()
+  request.open('post','/xxx')
+  request.setRequestHeader('frank','15')
+  request.send('我偏要设置request第四部分')
   request.onreadystatechange=function() {
     if (request.readyState===4) {
       console.log('请求响应完毕')
       console.log(request.status)
+      console.log(request.statusText)//可以获取到ok
       if (request.status>=200&&request.status<300) {
         console.log('请求成功了')
+        console.log(request.getResponseHeader('Content-Type'))
         console.log(request.responseText)
         let string=request.responseText
         let object=window.JSON.parse(string)
-        console.log(object)
       }else if (request.status>=400) {
         console.log('请求失败了')
       }
     }
   }
 })
+
+let script=document.createElement('script')
+document.body.appendChild('script')
+
+alert('1')
