6719c1d9113bd4a80e2815608d141b2e049a00d3
ycy <568082704@qq.com>
2019-07-31 17:16:21 +0800

    存个档

--- main.js
+++ main.js
@@ -1,27 +1,140 @@
-myButton.addEventListener('click',function() {
-  let request = new XMLHttpRequest
-  request.open('post','/xxx')
-  request.setRequestHeader('frank','15')
-  request.send('我偏要设置request第四部分')
-  request.onreadystatechange=function() {
+var jQuery=function() {}
+window.$=window.jQuery
+
+window.jQuery.ajax=function(options) {
+  let url=options.url
+  let method=options.method
+  let headers =options.headers
+  let body =options.body
+  let successFn=options.successFn
+  let failFn =options.failFn  
+  //我直接用变量先把对象里的东西拿出来，到时候写的时候就不用点了
+  let request=new XMLHttpRequest()
+  request.open(method,url)
+  for(let key in headers){
+    let value =headers[key]
+    request.setRequestHeader(key,value)
+  }
+  request.onreadystatechange=()=>{
     if (request.readyState===4) {
-      console.log('请求响应完毕')
-      console.log(request.status)
-      console.log(request.statusText)//可以获取到ok
       if (request.status>=200&&request.status<300) {
-        console.log('请求成功了')
-        console.log(request.getResponseHeader('Content-Type'))
-        console.log(request.responseText)
-        let string=request.responseText
-        let object=window.JSON.parse(string)
-      }else if (request.status>=400) {
-        console.log('请求失败了')
+        successFn.call(undefined,request.responseText)
+      }else if(request.status>=400){
+        failFn.call(undefined,request)
       }
     }
   }
-})
+  request.send(body)
+}
 
-let script=document.createElement('script')
-document.body.appendChild('script')
 
-alert('1')
+
+myButton.addEventListener('click',function() {
+  let obj={
+    url:'/frank',
+    method:'get',
+    headers:{
+      "Content-Type":"x-www-form-urlencoded",//要注意这里要用字符串
+      "frank":"18"
+    },
+    successFn:(e)=>{
+      console.log(e)
+    },
+    failFn:(e)=>{
+      console.log(e)
+      console.log(e.status)
+      console.log(e.responseText)
+    }
+  }
+  window.jQuery.ajax(obj)
+  })
+ 
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// myButton.addEventListener('click',function() {
+//   let request = new XMLHttpRequest
+//   request.open('post','/xxx')
+//   request.setRequestHeader('frank','15')
+//   request.send('我偏要设置request第四部分')
+//   request.onreadystatechange=function() {
+//     if (request.readyState===4) {
+//       console.log('请求响应完毕')
+//       console.log(request.status)
+//       console.log(request.statusText)//可以获取到ok
+//       if (request.status>=200&&request.status<300) {
+//         console.log('请求成功了')
+//         console.log(request.getResponseHeader('Content-Type'))
+//         console.log(request.responseText)
+//         let string=request.responseText
+//         let object=window.JSON.parse(string)
+//       }else if (request.status>=400) {
+//         console.log('请求失败了')
+//       }
+//     }
+//   }
+// })
+
+// let script=document.createElement('script')
+// document.body.appendChild('script')
+
+// alert('1')
